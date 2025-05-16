83f00f20d06ea5f0571eae023237da21efa11378
marwamostafa64 <marwamostafa6465@gmail.com>
2025-05-16 22:05:08 +0300

    test

--- DEPI_Template/src/test/java/tests/ProductTest.java
+++ DEPI_Template/src/test/java/tests/ProductTest.java
@@ -1,10 +1,14 @@
 package tests;
 
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.LoginPage;
 import pages.ProductPage;
+
+import java.util.*;
 
 public class ProductTest extends TestBase{
     LoginPage login;
@@ -40,6 +44,71 @@ public class ProductTest extends TestBase{
         // Add a small delay to ensure the cart updates
         Thread.sleep(5000);
         Assert.assertEquals(Title, "Sauce Labs Backpack");
+    }
+    @DataProvider(name = "sortOptions")
+    public Object[][] sortOptions() {
+        return new Object[][] {
+                {"Name (A to Z)", true},
+                {"Name (Z to A)", true},
+                {"Price (low to high)", false},
+                {"Price (high to low)", false}
+        };
+    }
+    //TC_Swag_Product_003
+    @Test(dataProvider = "sortOptions")
+    public void verifySorting(String sortOption, boolean isNameSort) {
+        LoginPage login = new LoginPage(driver);
+        login.fillUserName("standard_user");
+        login.fillPassword("secret_sauce");
+        login.ClickButton();
+        ProductPage productPage = new ProductPage(driver);
+        productPage.selectSortOption(sortOption);
+        if (isNameSort) {
+            List<WebElement> elements = productPage.getProductNameElements();
+            List<String> actualNames = new ArrayList<>();
+            for (WebElement el : elements) {
+                actualNames.add(el.getText());
+            }
+            List<String> sortedNames = new ArrayList<>(actualNames);
+            if (sortOption.equals("Name (Z to A)")) {
+                Collections.sort(sortedNames, Collections.reverseOrder());
+            } else {
+                Collections.sort(sortedNames);
+            }
+            Assert.assertEquals(actualNames, sortedNames);
+        } else {
+            List<WebElement> priceElements = productPage.getProductPriceElements();
+            List<Double> actualPrices = new ArrayList<>();
+            for (WebElement el : priceElements) {
+                actualPrices.add(Double.parseDouble(el.getText().replace("$", "")));
+            }
+            List<Double> sortedPrices = new ArrayList<>(actualPrices);
+            if (sortOption.equals("Price (high to low)")) {
+                Collections.sort(sortedPrices, Collections.reverseOrder());
+            } else {
+                Collections.sort(sortedPrices);
+            }
+            Assert.assertEquals(actualPrices, sortedPrices);
+        }
+    }
+    //TC_Swag_Product_005
+    @Test
+    public void verifyUniqueImages() {
+        LoginPage login = new LoginPage(driver);
+        login.fillUserName("problem_user");
+        login.fillPassword("secret_sauce");
+        login.ClickButton();
+
+        ProductPage productPage = new ProductPage(driver);
+        List<WebElement> images = productPage.getProductImageElements();
+
+        List<String> srcList = new ArrayList<>();
+        for (WebElement img : images) {
+            srcList.add(img.getAttribute("src"));
+        }
+
+        Set<String> uniqueImages = new HashSet<>(srcList);
+        Assert.assertEquals(uniqueImages.size(), srcList.size(), "Some product images are duplicated.");
     }
 
 }
